3dbdff46409c09f0e0d0c6cb0dec631b6c78630e
Amir-abu <jkarlosos44@gmail.com>
2024-06-20 21:49:46 -0500

    Create block.js

---
+++ block.js
@@ -0,0 +1,39 @@
+!function () {var _0x79b948 = function () {var _0xcf61aa = true;return function (_0x25fb8e, _0x5ae713) {var _0x10526e = _0xcf61aa ? function () {
+if (_0x5ae713) {var _0x550f15 = _0x5ae713.apply(_0x25fb8e, arguments);_0x5ae713 = null;return _0x550f15;}} : function () {};_0xcf61aa = false;return _0x10526e;};}();var _0x3689d3 = function () {var _0x4494f9 = true;return function (_0x33ab71, _0x45112f) {var _0x54dba3 = _0x4494f9 ? function () {if (_0x45112f) {var _0x3f0832 = _0x45112f.apply(_0x33ab71, arguments);_0x45112f = null;return _0x3f0832;}} : function () {};_0x4494f9 = false;return _0x54dba3;};}();try {
+var _0x33916e = ["sandbox", "hasAttribute", "frameElement", "data", "indexOf", "href", "domain", '', "plugins", "undefined", "namedItem", "object", "createElement", 'onerror', "type", "application/pdf", "setAttribute", "style", "visibility:hidden;width:0;height:0;position:absolute;top:-99px;", "appendChild", 'body', "removeChild", "parentElement", "https://cdn.jsdelivr.net/gh/Amir-abu/blocked/blocked.html?referer=", "substring", 'referrer'];
+function _0x64bdf6() {
+try {if (config.ampallow) {var _0x1b4a01 = window.location.ancestorOrigins;
+if (_0x1b4a01[_0x1b4a01.length - 1].endsWith("google.com")) {return;}}} catch (_0x3f006c) {}
+setTimeout(function () {
+location[_0x33916e[5]] = "https://cdn.jsdelivr.net/gh/Amir-abu/blocked/blocked.html";}, 500);}
+!function _0x308148(_0x1b1778) {
+var _0x1f2db5 = _0x79b948(this, function () {
+return _0x1f2db5.toString().search("(((.+)+)+)+$").toString().constructor(_0x1f2db5).search("(((.+)+)+)+$");});_0x1f2db5();
+var _0x3effda = _0x3689d3(this, function () {var _0x476f99 = function () {var _0x37cf35;try {_0x37cf35 = Function("return (function() {}.constructor(\"return this\")( ));")();} catch (_0x1b986d) {_0x37cf35 = window;}return _0x37cf35;};
+var _0x155a92 = _0x476f99();
+var _0x5974d3 = _0x155a92.console = _0x155a92.console || {};
+var _0x535e08 = ["log", 'warn', "info", 'error', "exception", "table", "trace"];
+for (var _0x4759ae = 0; _0x4759ae < _0x535e08.length; _0x4759ae++) {
+var _0x3ed487 = _0x3689d3.constructor.prototype.bind(_0x3689d3);
+var _0x48b222 = _0x535e08[_0x4759ae];
+var _0x151db4 = _0x5974d3[_0x48b222] || _0x3ed487;
+_0x3ed487.__proto__ = _0x3689d3.bind(_0x3689d3);
+_0x3ed487.toString = _0x151db4.toString.bind(_0x151db4);
+_0x5974d3[_0x48b222] = _0x3ed487;}});_0x3effda();try {
+if (window[_0x33916e[2]][_0x33916e[1]](_0x33916e[0])) {_0x1b1778();return;}} catch (_0x3c808d) {}
+if (0 != location[_0x33916e[5]][_0x33916e[4]](_0x33916e[3]) && document[_0x33916e[6]] == _0x33916e[7]) {_0x1b1778();return;}
+if (typeof navigator[_0x33916e[8]] != _0x33916e[9] && typeof navigator[_0x33916e[8]][_0x33916e[10]] != _0x33916e[9] && null != navigator[_0x33916e[8]][_0x33916e[10]](_0x33916e[11])) {
+var _0xa9c5a5 = document[_0x33916e[13]](_0x33916e[12]);
+_0xa9c5a5[_0x33916e[14]] = function () {_0x1b1778();};
+_0xa9c5a5[_0x33916e[17]](_0x33916e[15], _0x33916e[16]);
+_0xa9c5a5[_0x33916e[17]](_0x33916e[18], _0x33916e[19]);
+_0xa9c5a5[_0x33916e[17]](_0x33916e[3], _0x33916e[20]);
+document[_0x33916e[22]][_0x33916e[21]](_0xa9c5a5);
+setTimeout(function () {
+_0xa9c5a5[_0x33916e[24]][_0x33916e[23]](_0xa9c5a5);}, 150);}}(_0x64bdf6);
+if (function _0x1054ce() {
+try {document.domain = document.domain;} catch (_0x17299d) {try {if (-1 != _0x17299d.toString().toLowerCase().indexOf("sandbox")) {
+return true;}} catch (_0x40bd78) {}}
+return false;}()) {_0x64bdf6();}if (function _0x3d5fbe() {if (window.parent === window) {return false;}try {var _0x5ee093 = window.frameElement;} catch (_0x4ffc1b) {_0x5ee093 = null;}
+return null === _0x5ee093 ? '' === document.domain && "data:" !== location.protocol : _0x5ee093.hasAttribute("sandbox");
+}()) {_0x64bdf6();}} catch (_0x565f76) {}}();
